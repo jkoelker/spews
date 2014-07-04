b397ee2a5ff0818a982b6dd4badda1e2fada24ce
Jason Kölker <jason@koelker.net>
2014-07-04 01:53:15 +0000

    Fix indendation

--- setup.py
+++ setup.py
@@ -6,10 +6,9 @@ import setuptools as st
 st.setup(name='spew',
          packages=st.find_packages(),
          entry_points={
-            'afew.filter': [
-                'DistributionFilter= spew.distribution:DistributionFilter',
-                'StickyFilter = spew.sticky:StickyFilter',
-            ],
-        },
-        install_requires=['afew'],
-)
+             'afew.filter': [
+                 'DistributionFilter= spew.distribution:DistributionFilter',
+                 'StickyFilter = spew.sticky:StickyFilter',
+             ],
+         },
+         install_requires=['afew'])
--- spew/sticky.py
+++ spew/sticky.py
@@ -4,10 +4,10 @@ from afew.filters.BaseFilter import Filter
 
 
 class StickyFilter(Filter):
-     message = 'Looking for messages in threads that are not tagged consistant'
-     query = 'tag:inbox AND NOT tag:lists'
+    message = 'Looking for messages in threads that are not tagged consistant'
+    query = 'tag:inbox AND NOT tag:lists'
 
-     def handle_message(self, message):
+    def handle_message(self, message):
         tags = set()
         thread_id = message.get_thread_id()
 
